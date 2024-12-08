2321760054b0e734e7f605ed1ae631be0b3c82cd
reypryma <refanopradana@gmail.com>
2024-12-08 22:39:19 +0700

    repo

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -59,25 +59,3 @@ Future<List<String>> loadBanners() async {
   });
   return banner;
 }
-
-extension StringExtension on String? {
-  String? toCurrencyFormat({var format = '\$'}) {
-    return format + this;
-  }
-
-  String formatDateTime() {
-    if (this == null || this!.isEmpty || this == "null") {
-      return "NA";
-    } else {
-      return DateFormat("HH:mm dd MMM yyyy", "en_US").format(DateFormat("yyyy-MM-dd HH:mm:ss.0", "en_US").parse(this!));
-    }
-  }
-
-  String formatDate() {
-    if (this == null || this!.isEmpty || this == "null") {
-      return "NA";
-    } else {
-      return DateFormat("dd MMM yyyy", "en_US").format(DateFormat("yyyy-MM-dd", "en_US").parse(this!));
-    }
-  }
-}
