3467bdc61b745ac58be18ac6c49bdde0ac3f66d5
reypryma <refanopradana@gmail.com>
2024-12-22 23:20:02 +0700

    get color

--- lib/services/util.dart
+++ lib/services/util.dart
@@ -37,3 +37,19 @@ bool checkIfDeviceWide() => Responsive().deviceType() == "desktop" || Responsive
 void finish(BuildContext context, [Object? result]) {
   if (Navigator.canPop(context)) Navigator.pop(context, result);
 }
+
+Color getColorFromHex(String hexColor, {Color? defaultColor}) {
+  if (hexColor.isEmpty) {
+    if (defaultColor != null) {
+      return defaultColor;
+    } else {
+      throw ArgumentError('Can not parse provided hex $hexColor');
+    }
+  }
+
+  hexColor = hexColor.toUpperCase().replaceAll("#", "");
+  if (hexColor.length == 6) {
+    hexColor = "FF" + hexColor;
+  }
+  return Color(int.parse(hexColor, radix: 16));
+}
