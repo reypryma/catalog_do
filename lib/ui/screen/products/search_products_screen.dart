937d9b49e80acd30fba6327691555dc3558bb426
reypryma <refanopradana@gmail.com>
2024-12-24 15:19:51 +0700

    search done

--- lib/ui/screen/products/search_products_screen.dart
+++ lib/ui/screen/products/search_products_screen.dart
@@ -1,9 +1,16 @@
+import 'package:catalog_do/data/model/product.dart';
+import 'package:catalog_do/data/repository.dart';
 import 'package:catalog_do/layout/app_layout.dart';
+import 'package:catalog_do/theme/app_theme.dart';
+import 'package:catalog_do/ui/screen/products/components/search_not_found.dart';
+import 'package:catalog_do/ui/screen/products/components/search_products.dart';
+import 'package:catalog_do/ui/widgets/loader_widget.dart';
 import 'package:flutter/material.dart';
 
 import '../../../constant/constant.dart';
 import '../../../constant/style.dart';
 import '../../../layout/responsive.dart';
+import '../../widgets/search_catalog.dart';
 
 class SearchProductsScreen extends StatefulWidget {
   const SearchProductsScreen({super.key});
@@ -14,6 +21,36 @@ class SearchProductsScreen extends StatefulWidget {
 
 class _SearchProductsScreenState extends State<SearchProductsScreen> {
   final AppTheme _appTheme = AppTheme();
+  List<ShProduct> products = [];
+  List<ShProduct> filteredList = [];
+  bool _isLoading = true;
+
+  final TextEditingController _searchController = TextEditingController();
+  var searchText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future fetchData() async {
+    debugPrint("Search: $searchText");
+    _isLoading = true;
+    products.clear();
+    filteredList.clear();
+    products = await loadProducts();
+
+    for (var product in products) {
+      if (product.name!.toLowerCase().contains(searchText)) {
+        filteredList.add(product);
+      }
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,12 +86,14 @@ class _SearchProductsScreenState extends State<SearchProductsScreen> {
               constraints: Responsive().contentAreaWidth(),
               padding: EdgeInsets.all(8),
               decoration: BoxDecoration(
-                boxShadow:[BoxShadow(
-                  color: Colors.grey.withOpacity(0.2),
-                  blurRadius: 4.0,
-                  spreadRadius: 1.0,
-                  offset: const Offset(0.5, 0.0),
-                )],
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    blurRadius: 4.0,
+                    spreadRadius: 1.0,
+                    offset: const Offset(0.5, 0.0),
+                  )
+                ],
                 borderRadius: BorderRadius.all(Radius.circular(12)),
                 border: Border.all(
                     color: _appTheme.getTheme().colorScheme.primaryContainer,
@@ -62,33 +101,106 @@ class _SearchProductsScreenState extends State<SearchProductsScreen> {
                 color: _appTheme.getTheme().colorScheme.surfaceContainer,
               ),
               child: Padding(
-                  padding: Responsive().deviceType() == "desktop" ? sHorizontalGap : EdgeInsets.zero,
+                  padding: Responsive().deviceType() == "desktop"
+                      ? sHorizontalGap
+                      : EdgeInsets.zero,
                   child: Column(
                     children: [
-                      TextField(
-                        controller: TextEditingController(),
-                        obscureText: false,
-                        textAlign: TextAlign.start,
-                        maxLines: 1,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 16,
-                        ),
-                        decoration: searchCatalogDecoration(
-                          prefixIcon: Icon(Icons.search, color: _appTheme.getTheme().colorScheme.onSurfaceVariant,),
-                          showPreFixIcon: true,
-                          hintText: "Search product name",
-                        )
-                      ),
-                      SizedBox(height: 24,),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                                controller: _searchController,
+                                obscureText: false,
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 16,
+                                ),
+                                onChanged: (value) {
+                                  setState(() {
 
+                                  });
+                                },
+                                decoration: searchCatalogDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.search,
+                                    color: _appTheme
+                                        .getTheme()
+                                        .colorScheme
+                                        .onSurfaceVariant,
+                                  ),
+                                  showPreFixIcon: true,
+                                  hintText: "Search product name",
+                                  suffixIcon: _searchController.text.isNotEmpty
+                                      ? IconButton(
+                                          icon: Icon(
+                                            Icons.clear,
+                                            color: _appTheme
+                                                .getTheme()
+                                                .colorScheme
+                                                .onSurfaceVariant,
+                                          ),
+                                          onPressed: () {
+                                            _searchController.clear();
+                                            searchText = "";
+                                            fetchData();
+                                          },
+                                        )
+                                      : null,
+                                )),
+                          ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              searchText = _searchController.text;
+                              fetchData();
+                              setState(() {
+
+                              });
+                            },
+                            icon: Icon(Icons.send, size: 30),
+                          )
+                        ],
+                      ),
                     ],
-                  )
-              ),
+                  )),
             ),
+            SizedBox(height: 16,),
+            _showSearchedProduct()
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _showSearchedProduct() {
+    debugPrint("filter ${filteredList.length}");
+    return Container(
+      constraints: Responsive().contentAreaWidth(),
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+        border: Border.all(
+            color: _appTheme.getTheme().colorScheme.primaryContainer,
+            width: 0.5),
+        color: _appTheme.getTheme().colorScheme.surfaceContainer,
+      ),
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: _isLoading
+            ? LoaderWidget(
+                color: _appTheme.getTheme().colorScheme.primary,
+              )
+            : filteredList.isEmpty
+                ? SearchNotFound(message: _searchController.text)
+                : SearchProducts(
+                    products: filteredList,
+                  ),
       ),
     );
   }
--- lib/ui/widgets/search_catalog.dart
+++ lib/ui/widgets/search_catalog.dart
@@ -3,33 +3,70 @@ import 'package:catalog_do/ext/widget_extension.dart';
 import 'package:catalog_do/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-InputDecoration searchCatalogDecoration(
-    {Icon? icon, String? hintText, Icon? prefixIcon, bool showPreFixIcon = false}) {
+// InputDecoration searchCatalogDecoration(
+//     {Icon? icon, String? hintText, Icon? prefixIcon, bool showPreFixIcon = false}) {
+//   AppTheme appTheme = AppTheme();
+//
+//   return InputDecoration(
+//       contentPadding: EdgeInsets.all(16),
+//       border: OutlineInputBorder(
+//         borderRadius: BorderRadius.circular(8.0),
+//         borderSide: BorderSide(color: Colors.grey.withOpacity(0.6), width: 2),
+//       ),
+//       hintText: hintText,
+//       hintStyle: TextStyle(
+//         fontWeight: FontWeight.w500,
+//         fontSize: 16,
+//         color: appTheme.getTheme().colorScheme.onSurfaceVariant,
+//       ),
+//       focusedBorder: OutlineInputBorder(
+//         borderRadius: BorderRadius.circular(8.0),
+//         borderSide: BorderSide(
+//             color: appTheme.getTheme().colorScheme.tertiaryContainer),
+//       ),
+//       enabledBorder: OutlineInputBorder(
+//         borderRadius: BorderRadius.circular(8.0),
+//         borderSide: BorderSide(color: Colors.grey.withOpacity(0.4)),
+//       ),
+//       filled: true,
+//       fillColor: appTheme.getTheme().colorScheme.onSecondary,
+//       suffixIcon: icon.validate(),
+//       prefixIcon: showPreFixIcon ? prefixIcon?.validate() : null);
+// }
+
+InputDecoration searchCatalogDecoration({
+  Widget? suffixIcon,
+  String? hintText,
+  Icon? prefixIcon,
+  bool showPreFixIcon = false,
+}) {
   AppTheme appTheme = AppTheme();
 
   return InputDecoration(
-      contentPadding: EdgeInsets.all(16),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8.0),
-        borderSide: BorderSide(color: Colors.grey.withOpacity(0.6), width: 2),
+    contentPadding: EdgeInsets.all(16),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(color: Colors.grey.withOpacity(0.6), width: 2),
+    ),
+    hintText: hintText,
+    hintStyle: TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 16,
+      color: appTheme.getTheme().colorScheme.onSurfaceVariant,
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(
+        color: appTheme.getTheme().colorScheme.tertiaryContainer,
       ),
-      hintText: hintText,
-      hintStyle: TextStyle(
-        fontWeight: FontWeight.w500,
-        fontSize: 16,
-        color: appTheme.getTheme().colorScheme.onSurfaceVariant,
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8.0),
-        borderSide: BorderSide(
-            color: appTheme.getTheme().colorScheme.tertiaryContainer),
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8.0),
-        borderSide: BorderSide(color: Colors.grey.withOpacity(0.4)),
-      ),
-      filled: true,
-      fillColor: appTheme.getTheme().colorScheme.onSecondary,
-      suffixIcon: icon.validate(),
-      prefixIcon: showPreFixIcon ? prefixIcon?.validate() : null);
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(color: Colors.grey.withOpacity(0.4)),
+    ),
+    filled: true,
+    fillColor: appTheme.getTheme().colorScheme.onSecondary,
+    suffixIcon: suffixIcon, // Updated to Widget type
+    prefixIcon: showPreFixIcon ? prefixIcon : null,
+  );
 }
