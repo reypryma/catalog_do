c1d6f957874568e7b7e5a4e64bbb4f03164fd433
reypryma <refanopradana@gmail.com>
2024-12-26 13:06:24 +0700

    constant

--- lib/constant/variable_constant.dart
+++ lib/constant/variable_constant.dart
@@ -13,30 +13,15 @@ bool hideScaffoldAppBar = false;
 String appVersion = '1.0.0';
 String appContactEmail = 'email@example.com';
 
-// String defaultThemeName = 'Orange';
-// String defaultMode = 'light';
-// String defaultTopBar = 'primary';
-// String defaultMenu = 'light';
-
 Map<int, Map<String, dynamic>> bgSkins = {};
 int defaultBGSkin = 0;
 
-
-// Widgets-AppBackground2
-// String appBackground = "box"; //  "", box, inverted_box. Default "".
 double appBackgroundRadius = 30.0;
 double appBackgroundHeight = 200.0;
 double appBackgroundElevation = 3;
-double appBarElevation = 1; // Default 4.
+double appBarElevation = 1;
 
 // App Settings - Change Top Bar and Menu with App Mode
-bool linkTopBarWithMode = false;
-bool linkMenuWithMode = true;
-
 List<String> pageHeaderIcons = ["settings"];
 
 const bool enableFormValidation = true;
-
-const String apiURL = "";
-const String apiLocalDomain = "";
-const String apiLocalIP = "";
